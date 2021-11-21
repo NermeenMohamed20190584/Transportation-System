9235aa1be3dfbefe8bdf4ec5d2bbdbcbae91df33
NermeenMohamed20190584 <84995327+NermeenMohamed20190584@users.noreply.github.com>
2021-11-21 13:32:40 +0200

    Add files via upload

---
+++ src/transportation_sw/Account.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package transportation_sw;
+
+/**
+ *
+ * @author Tech Shop
+ */
+public class Account {
+    
+}
---
+++ src/transportation_sw/Transportation_SW.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package transportation_sw;
+
+/**
+ *
+ * @author Tech Shop
+ */
+public class Transportation_SW {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
